5974463e82320e5d4bca51db4b68cf75fce3f804
Pavel Krigin <pavel.krigin.85@gmail.com>
2023-03-17 23:27:12 +0100

    Initial Commit

---
+++ AnimatedButton/AnimatedButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedButtonApp.swift
+//  AnimatedButton
+//
+//  Created by Pavel Krigin on 17.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
