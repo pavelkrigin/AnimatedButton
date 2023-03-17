e3a62261459b7822801008aced69c65817a2b6b7
Pavel Krigin <pavel.krigin.85@gmail.com>
2023-03-17 23:37:28 +0100

    backgroundView_func_commit

--- AnimatedButton/ContentView.swift
+++ AnimatedButton/ContentView.swift
@@ -8,16 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: - Variables
+    @State private var offsetOne: CGSize = .zero
+    @State private var offsetTwo: CGSize = .zero
+    @State private var isCollapsed: Bool = false
+    
+    //MARK: - Body
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            backgroundView()
+            
         }
-        .padding()
     }
 }
+
+extension ContentView {
+    private func backgroundView() -> some View {
+        Rectangle()
+            .fill(.linearGradient(colors: [.black.opacity(0.9), .black, .black.opacity(0.9)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+    
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
