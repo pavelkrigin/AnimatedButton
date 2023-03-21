12df6000191e00da87cd5c1b7af458d1616d7ac7
Pavel Krigin <pavel.krigin.85@gmail.com>
2023-03-21 21:15:03 +0100

    final_commit

--- AnimatedButton/ContentView.swift
+++ AnimatedButton/ContentView.swift
@@ -17,11 +17,11 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             backgroundView()
-            
+            anmatedButton()
         }
     }
 }
-
+//MARK: - functions
 extension ContentView {
     private func backgroundView() -> some View {
         Rectangle()
@@ -37,7 +37,7 @@ extension ContentView {
             Rectangle()
                 .fill(.linearGradient(colors: [.purple, .pink], startPoint: .top, endPoint: .bottom))
                 .mask {
-                    Canvas{context, size
+                    Canvas{context, size in
                         context.addFilter(
                             .alphaThreshold(min: 0.8, color: .black))
                         context.addFilter(.blur(radius: 8))
@@ -54,12 +54,78 @@ extension ContentView {
                         }
                         
                     } symbols: {
-                        
+                        symbol(diameter: 120)
+                            .tag(1)
+                        symbol(offset: offsetOne)
+                            .tag(2)
+                        symbol(offset: offsetTwo)
+                            .tag(3)
                     }
                 }
+            cancelButton()
+                .blendMode(.softLight)
+                .rotationEffect(Angle(degrees: isCollapsed ? 90 : 45))
+            settingsButton()
+                .offset(offsetOne)
+                .blendMode(.softLight)
+                .opacity(isCollapsed ? 1 : 0)
+            homeButton()
+                .offset(offsetTwo)
+                .blendMode(.softLight)
+                .opacity(isCollapsed ? 1 : 0)
         }
-        
+        .frame(width: 120, height: 500)
+        .contentShape(Circle())
     }
+    
+    private func symbol(offset: CGSize =
+        .zero, diameter: CGFloat = 75) -> some View {
+            Circle()
+                .frame(width: diameter, height: diameter)
+                .offset(offset)
+        }
+    
+    private func homeButton() -> some View {
+        ZStack {
+            Image(systemName: "house")
+                .resizable()
+                .frame(width: 25, height: 25)
+        }
+        .frame(width: 65, height: 65)
+    }
+    
+    private func cancelButton() -> some View {
+        ZStack {
+            Image(systemName: "xmark")
+                .resizable()
+                .frame(width: 35, height: 35)
+                .aspectRatio(.zero, contentMode: .fit)
+                .contentShape(Circle())
+        }
+        .frame(width: 100, height: 100)
+        .contentShape(Rectangle())
+        .onTapGesture {
+            debugPrint("-- Cancel Tapped --")
+            withAnimation {
+                isCollapsed.toggle()
+            }
+            
+            withAnimation(.interactiveSpring(response: 0.35, dampingFraction: 0.8, blendDuration: 0.1).speed(0.5)) {
+                offsetOne = isCollapsed ? CGSize(width: 0, height: -120) : .zero
+                offsetTwo = isCollapsed ? CGSize(width: 0, height: -205) : .zero
+            }
+        }
+    }
+    
+    private func settingsButton() -> some View {
+        ZStack {
+            Image(systemName: "gear")
+                .resizable()
+                .frame(width: 28, height: 28)
+        }
+        .frame(width: 65, height: 65)
+    }
+        
 }
     
 
